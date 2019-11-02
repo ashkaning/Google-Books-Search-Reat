076345b0bdd11eb55d70bcf20f12c5f8f64c3a1a
ashkan <ashkan.ram70@gmail.com>
2019-11-02 09:46:28 -0700

    Updates

---
+++ precache-manifest.fdf8d957d97263ee97579fa1ed26613b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d2f055f5a7dad923d73",
+    "url": "/Google-Books-Search-Reat/static/js/runtime~main.533b082c.js"
+  },
+  {
+    "revision": "1aaf5bf614da0f09fe44",
+    "url": "/Google-Books-Search-Reat/static/js/main.fc8421c7.chunk.js"
+  },
+  {
+    "revision": "a9262bc7a2e0384350aa",
+    "url": "/Google-Books-Search-Reat/static/js/2.3a997784.chunk.js"
+  },
+  {
+    "revision": "1aaf5bf614da0f09fe44",
+    "url": "/Google-Books-Search-Reat/static/css/main.b5962b6c.chunk.css"
+  },
+  {
+    "revision": "0fc2215404b9b1e8ac8ede94eafd8266",
+    "url": "/Google-Books-Search-Reat/index.html"
+  }
+];
